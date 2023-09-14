268c22757a7f3263e706493ce7beeb786e8d1647
matszpk <matszpk@interia.pl>
2023-09-14 17:30:27 +0200

    Next testcase.

--- src/lib.rs
+++ src/lib.rs
@@ -3411,6 +3411,43 @@ mod tests {
             )
         );
 
+        assert_eq!(
+            ClauseCircuit::new(
+                9,
+                [
+                    Clause::new_xor([(7, false), (3, false), (5, false)]),
+                    Clause::new_and([
+                        (4, true),
+                        (6, true),
+                        (8, true),
+                        (2, false),
+                        (0, false),
+                        (1, false),
+                        (9, false)
+                    ]),
+                ],
+                [(10, false)]
+            )
+            .unwrap(),
+            ClauseCircuit::from(
+                Circuit::new(
+                    9,
+                    [
+                        Gate::new_and(0, 1),
+                        Gate::new_and(2, 9),
+                        Gate::new_xor(3, 5),
+                        Gate::new_xor(7, 11),
+                        Gate::new_nor(4, 6),
+                        Gate::new_nimpl(13, 8),
+                        Gate::new_and(10, 12),
+                        Gate::new_and(14, 15),
+                    ],
+                    [(16, false)]
+                )
+                .unwrap(),
+            )
+        );
+
         // evaluation test
         let circuit = Circuit::new(
             3,
