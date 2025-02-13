bf7dceab8aa079192263908c911a1af96a3ca452
matszpk <matszpk@interia.pl>
2025-02-13 17:41:29 +0100

    Add documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,53 @@
+#![cfg_attr(docsrs, feature(doc_cfg))]
+//! The base library of the Gate Project. It provides basic types of circuits and basic
+//! methods to operate on that circuits. The library doesn't provide complex simulation
+//! routines. This library is really base of the Gate Project.
+//!
+//! The basic type of circuit (`Circuit`) constructed from gates of following types:
+//! * 'and' - AND gate that returns true if two inputs are true, otherwise returns false.
+//! * 'nor' - NOR (negation of OR) gate that returns true if two inputs are false,
+//!    otherwise returns false.
+//! * 'nimpl' - NIMPL (negation of implication) gate that returns true if first input is true
+//!    and second is false, otherwise it returns false.
+//! * 'xor' - XOR gate that return true if inputs are different.
+//!
+//! The circuit defined by input length (number), gates and outputs that are defined by
+//! wire and negation. If wire is index of input or output of gate.
+//! The input of circuit starts from 0. The output of gate starts from input length number.
+//! The circuit can be constructed from gates satisfied following constraints:
+//! * All inputs for all gates and output wires are correct types
+//!   (input is less than current gate output wire index).
+//! * All inputs are used by some gates or circuit outputs.
+//! * All gate outputs are used some gates or circuit outputs.
+//!
+//! Additional type of circuit `(ClauseCircuit)` constructed from clauses. The clause is
+//! gate that uses any number of inputs. The are two clause types:
+//! * 'and' clause that returns true if all inputs are true.
+//! * 'xor' clause that returns true if number of inputs of true value is odd.
+//!
+//! Similary, the clause circuit defined by input length (number),
+//! clauses and outputs that are defined by wire and negation.
+//! If wire is index of input or output of gate.
+//! The input of circuit starts from 0. The output of gate starts from input length number.
+//! The circuit can be constructed from gates satisfied following constraints:
+//! * All inputs for all clauses and output wires are correct types
+//!   (input is less than current clause output wire index).
+//! * All inputs are used by some clauses or circuit outputs.
+//! * All clauses outputs are used some clauses or circuit outputs.
+//!
+//! Derived QuantCircuit from Circuit provides additional information about quantifiers.
+//! These quantifiers can be used to evaluate or solve quantified formula defined by circuit.
+//! There are two types of quantifiers:
+//! * 'all' - formula is satisified if circuit returns true for all combinations of inputs.
+//! * 'exists' - formula is satisified if circuit returns true for some combinations of inputs.
+//!
+//! Number of quantifiers must match to input length. Any sequence of quantifiers for inputs
+//! determine single quantifier for some input length. Number of output must be 1.
+//!
+//! For describe: `[all,all,exists,exists,all,all]` - formula is true if for all combinations of
+//! input0, input1 can be found some combination of input2,input3 all combinations
+//! input4, input5 circuit returns true.
+
 use std::cmp::{Ord, Ordering, PartialOrd};
 use std::fmt::{self, Debug, Display, Formatter};
 use std::hash::Hash;
